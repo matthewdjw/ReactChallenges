8bebb9c08c3fb9f7bf111708ba56ca1e38d871e9
matthewdjw <matthewdjw@gmail.com>
2024-10-19 14:49:02 -0400

    List challenges
    Listing challenges with sample data

--- components/challenges/ChallengeCard.tsx
+++ components/challenges/ChallengeCard.tsx
@@ -1,9 +1,37 @@
 import React, { FC } from 'react';
 
-interface ChallengeCardProps {}
+interface Challenge {
+	title: string;
+	description: string;
+	difficulty: 'Easy' | 'Medium' | 'Hard';
+	tags: string[];
+	solution: string;
+}
 
-const ChallengeCard: FC<ChallengeCardProps> = ({}) => {
-	return <div>ChallengeCard</div>;
+interface ChallengeCardProps {
+	challenge: Challenge;
+}
+
+const ChallengeCard: FC<ChallengeCardProps> = ({ challenge }) => {
+	return (
+		<div className="bg-white shadow-md rounded-lg p-6 m-4">
+			<h1 className="text-2xl font-bold text-gray-800 mb-2">{challenge.title}</h1>
+			<p className="text-sm text-gray-600 mb-2">
+				<span className="font-semibold">Difficulty: </span>
+				<span
+					className={`${challenge.difficulty === 'Easy'
+						? 'text-green-500'
+						: challenge.difficulty === 'Medium' ? 'text-yellow-500' : 'text-red-500'}`}
+				>
+					{challenge.difficulty}
+				</span>
+			</p>
+			<p className="text-sm text-gray-600">
+				<span className="font-semibold">Tags: </span>
+				<span className="text-blue-500">{challenge.tags.join(', ')}</span>
+			</p>
+		</div>
+	);
 };
 
 export default ChallengeCard;
--- pages/challenges.tsx
+++ pages/challenges.tsx
@@ -1,3 +1,43 @@
-export default function Challenges() {
-	return <div />;
+import ChallengeCard from '@/components/challenges/ChallengeCard';
+import Navbar from '@/components/common/Navbar';
+import fs from 'fs';
+import path from 'path';
+import { GetServerSideProps } from 'next';
+
+interface Challenge {
+	title: string;
+	description: string;
+	difficulty: 'Easy' | 'Medium' | 'Hard';
+	tags: string[];
+	solution: string;
 }
+
+interface ChallengesPageProps {
+	challenges: Challenge[];
+}
+
+const Challenges: React.FC<ChallengesPageProps> = ({ challenges }) => {
+	return (
+		<div>
+			<Navbar />
+			{challenges.map((challenge) => (
+				<div key={challenge.title}>
+					<ChallengeCard challenge={challenge} />
+				</div>
+			))}
+		</div>
+	);
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+	const filePath = path.join(process.cwd(), 'public/data/challenges', 'challenge-sample.json');
+	const jsonData = fs.readFileSync(filePath);
+	const challenges: Challenge[] = JSON.parse(jsonData.toString());
+	return {
+		props: {
+			challenges
+		}
+	};
+};
+
+export default Challenges;
