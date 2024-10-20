d27452afa4485b7432ec37ab2b9b3e15acf7521a
matthewdjw <matthewdjw@gmail.com>
2024-10-20 19:27:10 -0400

    Update navbar
    Small visual update

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -1,27 +1,27 @@
-import React, { FC } from 'react';
-import Link from 'next/link';
+import React, { FC } from "react";
+import Link from "next/link";
 interface NavbarProps {}
 
 const Navbar: FC<NavbarProps> = () => {
-	return (
-		<nav className="p-4">
-			<div className="container mx-auto flex justify-between items-center">
-				<div className="space-x-6">
-					<Link href="/" className="text-gray-300 hover:text-white">
-						Home
-					</Link>
-					<Link href="/challenges" className="text-gray-300 hover:text-white">
-						Challenges
-					</Link>
-				</div>
+  return (
+    <nav className="mx-6 mt-4">
+      <div className="flex">
+        <div className="space-x-6">
+          <Link href="/" className="text-gray-300 hover:text-white">
+            Home
+          </Link>
+          <Link href="/challenges" className="text-gray-300 hover:text-white">
+            Challenges
+          </Link>
+        </div>
 
-				{/* Call to action button */}
-				<a href="#" className="py-2 px-4 bg-indigo-600 text-white rounded-lg hover:bg-indigo-500">
+        {/* Sign In */}
+        {/*<a href="#" className="py-2 px-4 bg-indigo-600 text-white rounded-lg hover:bg-indigo-500">
 					Sign In
-				</a>
-			</div>
-		</nav>
-	);
+				</a>*/}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
